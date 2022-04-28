330292d10e4feef9b34c6ac8790c4e1349b0b4b1
minghan <a5534222@gmail.com>
2022-04-28 17:24:50 +0800

    完美新增大老二功能

--- Git01/Form1.Designer.cs
+++ Git01/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Git01
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,11 +52,22 @@ namespace Git01
             this.label1.Size = new System.Drawing.Size(0, 19);
             this.label1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "要看大老二點我";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,6 +81,7 @@ namespace Git01
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
